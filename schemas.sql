43d3769bc0507dbe3d5aa2055a72e2d76ca92b9c
=armada6199 <moh.abdin27@gmail.com>
2023-05-23 10:59:46 +0300

    removing the server

--- schemas.sql
+++
@@ -1,8 +0,0 @@
--- create table  movies(
---     id serial primary key,
---     movieId integer,
---     title varchar(255),
---     relase_date integer,
---     comments varchar(10000),
---     rating integer
--- );
